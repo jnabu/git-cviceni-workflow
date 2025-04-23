6da3b6c7f5dcee7c32be987c7ab25aa16f84cf41
Judita Buchlovská Nagyová <juddy13@gmail.com>
2025-04-23 19:09:13 +0200

    Modified kalkulacka.py to add nasob function

--- kalkulacka.py
+++ kalkulacka.py
@@ -5,6 +5,12 @@ def secti(a, b):
 def odecti(a, b):
 	return a-b
 
+
+def nasob(a, b):
+	return a*b
+
 print(f”2 + 3 = {secti(2, 3)}”)
 
 print(f”5 - 1 = {odecti(5, 1)}”)
+
+print(f”5 * 6 = {nasob(5, 6)}”)
