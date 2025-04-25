84b766d567d5548aa307af64ec8918576067d711
Judita Buchlovská Nagyová <juddy13@gmail.com>
2025-04-25 11:13:33 +0200

    Modified schema.sql

--- schema.sql
+++ schema.sql
@@ -3,3 +3,12 @@ CREATE TABLE uzivatel (
     username VARCHAR(50) NOT NULL,
     email VARCHAR(100)
 );
+
+CREATE TABLE project (
+    project_id INT PRIMARY KEY,
+    project_name VARCHAR(100) NOT NULL,
+    description TEXT,
+    start_date DATE,
+    end_date DATE,
+    status VARCHAR(20)
+);
